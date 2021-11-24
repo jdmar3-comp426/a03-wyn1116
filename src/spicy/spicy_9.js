3efe393e2680cd334250fb700fbce17b8661727b
wyn1116 <wyn342@gmail.com>
2021-11-24 07:13:34 -0500

    Initial implementation of multiplyBy, etc.

--- src/spicy/spicy_9.js
+++ src/spicy/spicy_9.js
@@ -49,7 +49,12 @@ export const repeatDemo = () => {
  *   product of num1 and num2.
  */
 export const multiplyBy = (num1) => {
+    function multiply(num2) {
+        return num1 * num2;
+    }
 
+    let func = multiply;
+    return func;
 };
 
 
@@ -57,7 +62,7 @@ export const multiplyBy = (num1) => {
  * Use the multiplyBy function to create and export a function named
  *   "tenTimes" that multiplies a number by 10.
  */
-export const tenTimes = undefined;
+export const tenTimes = multiplyBy(10);
 
 
 /**
@@ -65,7 +70,7 @@ export const tenTimes = undefined;
  *   function to multiply 50 by 10 and returns the result.
  */
 export const tenTimesFifty = () => {
-
+    return tenTimes(50);
 };
 
 
